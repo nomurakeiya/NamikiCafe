b19c9bb684981f07c04f8db7634e8b75363216e6
nomura_keiya <nomunomukeiya@gmail.com>
2021-03-07 12:14:19 +0900

    first-commit

---
+++ main.js
@@ -0,0 +1,15 @@
+lottie.loadAnimation({
+    container: document.getElementById("first"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "first.json"
+  });
+
+lottie.loadAnimation({
+  container: document.getElementById("second"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "second.json"
+});
